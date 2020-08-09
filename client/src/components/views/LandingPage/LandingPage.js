eeb8617f926407adcd8e6d3f37c26f0c5237e4f6
Lee, Jung Geun <cjred77@gmail.com>
2020-08-09 14:32:01 +0900

    load more

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,22 +1,89 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
-import Axios from 'axios';
+import axios from 'axios';
+import { Button, Icon, Card, Row, Col} from 'antd'
+import Meta from 'antd/lib/card/Meta'
+import ImageSlider from '../../utils/ImageSlider';
 
 function LandingPage() {
 
+    const [Products, setProducts] = useState([])
+    const [Skip, setSkip] = useState(0)
+    const [Limit, setLimit] = useState(2)
+    const [PostSize, setPostSize] = useState(0)
+
     useEffect(() => {
-        Axios.post('/api/product/products')
+
+        let body = {
+            skip: Skip,
+            limit: Limit
+        }
+        getProducts(body)
+    }, [])
+
+    const getProducts = (body) => {
+        axios.post('/api/product/products', body)
         .then(response => {
             if(response.data.success){
-                console.log(response.data)
+                //console.log(response.data)
+                if(body.loadMore){
+                    setProducts([...Products, ...response.data.productInfo])
+                } else {
+                    setProducts(response.data.productInfo)
+                }
+                setPostSize(response.data.postSize)
             } else {
                 alert(" 상품들을 가져오는데 실패하였습니다.")
             }
         })
-    }, [])
+    }
+
+    const loadMoreHandler = () => {
+        let skip = Skip + Limit
+
+        let body = {
+            skip: skip,
+            limit: Limit,
+            loadMore: true
+        }
+        getProducts(body)
+        setSkip(skip)
+    }
+
+    const renderCards = Products.map((product, index) => {
+        console.log(product)
+        return <Col lg={6} md={8} xs={24} key={index}>
+            <Card
+                cover={<ImageSlider images={product.images}/>}
+            >
+                <Meta 
+                    title={product.title}
+                    description={`$${product.price}`}
+                />
+            </Card>
+        </Col>
+    })
     return (
-        <div>
-            LandingPage
+        <div style={{ width: '75%', margin: '3rem auto'}}>
+            <div style={{ textAlign: 'center' }}>
+                <h2>Let's Travel Anywhere <Icon type="rocket" /></h2>
+            </div>
+
+            {/* Filter */}
+
+            {/* Search */}
+
+            {/* Cards */}
+            <Row gutter={[16, 16]} >
+                {renderCards}
+            </Row>
+
+            <br />
+            {PostSize >= Limit &&
+                <div style={{ justifyContent: 'center' }}>
+                    <button onClick={loadMoreHandler}> 더보기 </button>
+                </div>
+            }
         </div>
     )
 }
